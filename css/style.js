de8bbffb460b39ad5cee6f160d9c49457cbea08a
mannycolon <colonmanuel7@gmail.com>
2017-01-12 19:05:19 -0500

    moved all css code to style.js file

--- css/style.js
+++ css/style.js
@@ -6,7 +6,23 @@ var style = {
     minHeight: "100%",
     maxHeight: "100%",
     backgroundColor: "#c4c4c4"
-  }
+  },
+  tHGlyphicon: {
+    float: "right",
+    color: "#333333",
+    fontSize: "20px",
+    cursor: "pointer"
+  },
+  modalTitle:{
+    textAlign: "center",
+    color: "#FFFFFF"
+  },
+  tHModalContent:{
+    fontSize: "18px",
+    backgroundColor: "#333333",
+    color: "#FFFFFF",
+    padding: "45px 80px"
+  },
 };
 
 module.exports = style;
