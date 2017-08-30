41759a11a1ddac3008335bc1d45fac770d09e3c7
mannycolon <colonmanuel7@gmail.com>
2017-08-30 11:41:14 -0400

    changed the way we export the namespace and container from the tool to tC

--- Container.js
+++ Container.js
@@ -4,13 +4,12 @@ import fs from 'fs-extra';
 import {ipcRenderer} from 'electron';
 // constant declaration
 const urlRegex = new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})");
-const NAMESPACE = 'TranslationHelps';
 
 class Container extends React.Component {
   constructor(){
     super();
     this.state = {
-      modalVisibility: false,
+      modalVisibility: false
     };
     this.followLink = this.followLink.bind(this);
   }
@@ -105,7 +104,4 @@ class Container extends React.Component {
   }
 }
 
-module.exports = {
-    name: NAMESPACE,
-    container: Container
-}
+export default Container;
