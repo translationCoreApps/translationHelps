4576475d7771f25e66db7a38b41f5770aa6e27c8
Richard Mahn <rmahn@getmealticket.com>
2018-03-09 11:37:57 -0700

    Properly tests THelpsModal

--- __tests__/THelpsModal.test.js
+++ __tests__/THelpsModal.test.js
@@ -1,10 +1,7 @@
 /* eslint-env jest */
 import React from 'react';
 import THelpsModal from '../src/components/THelpsModal';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import renderer from 'react-test-renderer';
-
-jest.mock('../src/components/THelpsModal.js', () => '[THelpsModal]');
+import { shallow } from 'enzyme';
 
 describe('THelpsModal component Tests', () => {
   it('Check THelpsModal component', () => {
@@ -14,12 +11,7 @@ describe('THelpsModal component Tests', () => {
       children: "[children]",
       show: true
     };
-    const component = renderer.create(
-      <MuiThemeProvider>
-        <THelpsModal {...props} />
-      </MuiThemeProvider>
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const wrapper = shallow(<THelpsModal {...props} />);
+    expect(wrapper.prop('children')).toMatchSnapshot();
   });
 });
--- src/Container.js
+++ src/Container.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {ipcRenderer} from 'electron';
 import View from './components/View';
 import * as tHelpsHelpers from './helpers/tHelpsHelpers';
 
